1c39f8dc0c341e9afea033df8b76a1db33952b17
PaulaScharf <paulascharf@gmx.de>
2024-07-19 16:26:01 +0200

    add attributes and translation

--- src/lib/sensors/overtaking-prediction.tsx
+++ src/lib/sensors/overtaking-prediction.tsx
@@ -1,5 +1,6 @@
 import AbstractSensor from './abstract-sensor'
 import BaseSensor from './base-sensor'
+import i18n from '@/i18n'
 
 export default class OvertakingPredictionSensor<T extends [number, number]>
   extends BaseSensor<T>
@@ -9,6 +10,7 @@ export default class OvertakingPredictionSensor<T extends [number, number]>
     'FC01C688-2C44-4965-AE18-373AF9FED18D'
 
   public static type: string = 'overtaking'
+  public static attributes: string[] = [i18n.t('phenomena.overtaking_car'), i18n.t('phenomena.overtaking_bike')]
 
   parseData(data: DataView): T {
     const [prediction_car, prediction_bike] = this.parsePackages(data)
